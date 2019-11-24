fe9b96ba383ad1eaee8fad5991674f8b436bd863
LifeOfJona <LifeOfJona@hotmail.com>
2019-11-24 14:54:01 -0500

    Planet Models added to Scene.

--- main.js
+++ main.js
@@ -70,41 +70,126 @@ var ambient = new THREE.AmbientLight(0x222222);
 scene.add(ambient);
 
 
-
-
-// ------------------------------------------------------------
+// Solar System Models Added Here.
+// ---------------------------------------------------------
 const loader = new THREE.TextureLoader();
 
-
 //Sun
-var sun, gun_geom, sun_mat;
-sun_geom = new THREE.SphereGeometry(2300, 80, 80);
-sun_mat = new THREE.MeshPhongMaterial({
+var sun_geom = new THREE.SphereGeometry(2300, 80, 80);
+var sun_material = new THREE.MeshPhongMaterial({
     emissive: 0xffffff,
-    emissiveMap: loader.load('sunmap.jpg'),
+    emissiveMap: loader.load('images/sun.jpg'),
 });
-sun = new THREE.Mesh(sun_geom, sun_mat);
+var sun = new THREE.Mesh(sun_geom, sun_material);
+
 scene.add(sun);
 
 
-var geometry = new THREE.SphereGeometry(2300, 80, 80);
-var texture2 = loader.load('https://i.imgur.com/BpldqPj.jpg');
-
-var material = new THREE.MeshPhongMaterial({
-    emissiveMap: texture2,
+// Earth.
+var earth_geom = new THREE.SphereGeometry(1000, 80, 80);
+var earth_material = new THREE.MeshPhongMaterial({
     emissive: 0xffffff,
+    emissiveMap: loader.load('images/earthmap1k.jpg')
 });
-var earth = new THREE.Mesh(geometry, material);
-earth.position.set(5000, 0, 0);
+var earth = new THREE.Mesh(earth_geom, earth_material);
+earth.position.set(8500, 0, 0);
 
 scene.add(earth);
 
+
+// Moon 
+var moon_geom = new THREE.SphereGeometry(500, 50, 50);
+var moon_material = new THREE.MeshPhongMaterial({
+    emissive: 0xffffff,
+    emissiveMap: loader.load('images/moonmap1k.jpg')
+});
+var moon = new THREE.Mesh(moon_geom, moon_material);
+moon.position.set(10000, 5000, 0);
+
+scene.add(moon);
+
+// Mercury
+var mercury_geom = new THREE.SphereGeometry(1000,80,80);
+var mercury_material = new THREE.MeshPhongMaterial({
+    emissive: 0xffffff,
+    emissiveMap: loader.load('images/mercurymap.jpg')
+});
+var mercury = new THREE.Mesh(mercury_geom, mercury_material);
+mercury.position.set(3500, 0, 0);
+
+scene.add(mercury);
+
+
+// Venus
+var venus_geom = new THREE.SphereGeometry(1000,80,80);
+var venus_material = new THREE.MeshPhongMaterial({
+    emissive: 0xffffff,
+    emissiveMap: loader.load('images/venusmap.jpg')
+});
+var venus = new THREE.Mesh(venus_geom, venus_material);
+venus.position.set(6000, 0, 0);
+
+scene.add(venus);
+
+// Mars
+var mars_geom = new THREE.SphereGeometry(1000,80,80);
+var mars_material = new THREE.MeshPhongMaterial({
+    emissive: 0xffffff,
+    emissiveMap: loader.load('images/mars_1k_color.jpg')
+});
+var mars = new THREE.Mesh(mars_geom, mars_material);
+mars.position.set(11000, 0, 0);
+
+scene.add(mars);
+
+// Jupiter
+var jupiter_geom = new THREE.SphereGeometry(1000,80,80);
+var jupiter_material = new THREE.MeshPhongMaterial({
+    emissive: 0xffffff,
+    emissiveMap: loader.load('images/jupitermap.jpg')
+});
+var jupiter = new THREE.Mesh(jupiter_geom, jupiter_material);
+jupiter.position.set(13500, 0, 0);
+
+scene.add(jupiter);
+
+
+// Saturn
+var saturn_geom = new THREE.SphereGeometry(1000,80,80);
+var saturn_material = new THREE.MeshPhongMaterial({
+    emissive: 0xffffff,
+    emissiveMap: loader.load('images/saturnmap.jpg')
+});
+var saturn = new THREE.Mesh(saturn_geom, saturn_material);
+saturn.position.set(16000, 0, 0);
+
+scene.add(saturn);
+
+// Saturn Ring
+/*
+var ring_geo = new THREE.RingGeometry(1000,5,32);
+var ring_material = new THREE.MeshPhongMaterial({
+    emissive: 0xffffff,
+    emissiveMap: loader.load('images/saturnringcolor.jpg'),
+    side: THREE.DoubleSide
+});
+var saturnRing = new THREE.Mesh(ring_geo, ring_material);
+saturnRing.position.set(20000, 0, 0);
+
+scene.add(saturnRing);
+*/
+
+
+
+
+
+//----------------------------------------------------------------
+
 var t = 0;
-
-
+var yx = 0;
 
 document.addEventListener('mousemove', function (event) {
-    y = parseInt(event.offsetY);
+    yx = parseInt(event.offsetY);
 
 });
 
@@ -116,14 +201,22 @@ function animate() {
     requestAnimationFrame(animate);
 
     sun.rotation.y += 0.001;
-    earth.rotation.y += 1 / 16 * delta;
+    earth.rotation.y += 0.002;
+    moon.rotation.y += 0.002;
+    mercury.rotation.y+= 0.002;
+    venus.rotation.y+=0.002;
+    mars.rotation.y +=0.002;
+    jupiter.rotation.y +=0.002;
+    saturn.rotation.y +=0.002;
 
-    //camera.position.y = y * 5;
+
+    //earth.rotation.y += 1 / 16 * delta;
+
+    // Uncomment to move screen with cursor.
+    //camera.position.y = yx * 5;
+
+    //t += Math.PI / 180 * 2;
     camera.lookAt(scene.position);
-
-
-    t += Math.PI / 180 * 2;
-
     renderer.render(scene, camera);
 }
 
